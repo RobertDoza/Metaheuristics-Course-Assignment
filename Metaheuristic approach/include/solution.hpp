1175547d44ed35634146872324012fe3c7c57d05
Robert <dozarobert98@gmail.com>
2024-04-24 11:05:48 +0200

    Add class members and constructor to Solution class

--- Metaheuristic approach/include/solution.hpp
+++ Metaheuristic approach/include/solution.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <vector>
+
 class Solution {
-	// TODO
+	public:
+		Solution(int, int);
+	private:
+		const int _j;
+		const int _t;
+		std::vector<bool> _vector;
 };
--- Metaheuristic approach/src/solution.cpp
+++ Metaheuristic approach/src/solution.cpp
@@ -1,1 +1,5 @@
 #include "solution.hpp"
+
+Solution::Solution(int j, int t)
+	:_j(j), _t(t), _vector(j*t)
+{}
