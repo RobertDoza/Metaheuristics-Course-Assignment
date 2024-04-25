2132414989be891f500657ae6de3cd55fc8e98eb
Robert <dozarobert98@gmail.com>
2024-04-25 18:56:02 +0200

    Implement RandomGenerator class

--- Metaheuristic approach/include/random.hpp
+++ Metaheuristic approach/include/random.hpp
@@ -1,5 +1,22 @@
 #pragma once
 
-class Random {
-	// TODO
+#include <random>
+
+class RandomGenerator {
+	public:
+		static void set_seed(unsigned);
+		static unsigned get_seed();
+		
+		static int get_random_number(int);
+	private:
+		std::mt19937 _generator;
+		unsigned _seed;
+		
+		RandomGenerator();
+		
+		RandomGenerator(const RandomGenerator&) = delete;
+		RandomGenerator& operator=(const RandomGenerator&) = delete;
+		
+		static RandomGenerator& get_instance();
 };
+
--- Metaheuristic approach/src/random.cpp
+++ Metaheuristic approach/src/random.cpp
@@ -1,1 +1,26 @@
 #include "random.hpp"
+
+void RandomGenerator::set_seed(unsigned seed) {
+	get_instance()._seed = seed;
+	get_instance()._generator.seed(seed);
+}
+
+unsigned RandomGenerator::get_seed() {
+	return get_instance()._seed;
+}
+
+int RandomGenerator::get_random_number(int n) {
+	std::uniform_int_distribution<int> distribution(0, n - 1);
+	
+	return distribution(get_instance()._generator);
+}
+
+RandomGenerator::RandomGenerator()
+	:_generator(std::random_device{}())
+{}
+
+RandomGenerator& RandomGenerator::get_instance() {
+	static RandomGenerator generator;
+	return generator;
+}
+
