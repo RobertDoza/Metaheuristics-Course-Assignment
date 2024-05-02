ba832055808eb476391913826209871a385e73fd
Robert <dozarobert98@gmail.com>
2024-05-02 17:50:45 +0200

    Add InputReader class

---
+++ Metaheuristic approach/include/input_reader.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+class InputReader {
+	static void read_input(const std::string&);
+};
---
+++ Metaheuristic approach/src/input_reader.cpp
@@ -0,0 +1,5 @@
+#include "input_reader.hpp"
+
+void InputReader::read_input(const std::string&) {
+	// TODO
+}
