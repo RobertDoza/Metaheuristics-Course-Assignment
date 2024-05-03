f20edb6f1b935fda346e080962e63b6a81710fbb
Robert <dozarobert98@gmail.com>
2024-05-03 12:39:44 +0200

    Create signleton pattern for Model class

--- Metaheuristic approach/include/fitness.hpp
+++ Metaheuristic approach/include/fitness.hpp
@@ -6,13 +6,22 @@
 
 class Model {
 	public:
+		static Model& get_model();
 		double calculate_fitness(const Solution&) const;
 	private:
+		static Model model;
+		static bool created;
+		
+		Model();
+		Model(int, int, int, double, int, const std::vector<std::vector<double>>&, const std::vector<std::vector<double>>&);
+		Model(const Model&) = delete;
+		Model& operator=(const Model&) = delete;
+		
 		int _i;
 		int _j;
 		int _t;
 		double _s;
 		int _p;
 		std::vector<std::vector<double>> _population_matrix;
-		std::vector<std::vector<double>> _destination_matrix;
+		std::vector<std::vector<double>> _distance_matrix;
 };
--- Metaheuristic approach/src/fitness.cpp
+++ Metaheuristic approach/src/fitness.cpp
@@ -1,6 +1,22 @@
 #include "fitness.hpp"
 
+bool Model::created = false;
+Model Model::model;
+
+Model& Model::get_model() {
+	if (!created) {
+		throw std::logic_error("Model not created");
+	}
+	return model;
+}
+
 double Model::calculate_fitness(const Solution&) const {
 	// TODO
 	return 3.14;
 }
+
+Model::Model() {}
+
+Model::Model(int i, int j, int t, double s, int p, const std::vector<std::vector<double>>& pop, const std::vector<std::vector<double>>& dist)
+	:_i(i), _j(j), _t(t), _s(s), _p(p), _population_matrix(pop), _distance_matrix(dist)
+{}
