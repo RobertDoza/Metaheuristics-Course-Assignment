ce934cd11c734d64bf2ca3e98ef9cb074a8fb2a2
Robert <dozarobert98@gmail.com>
2024-12-13 20:56:55 +0100

    Refactor main.cpp
    - Moved code that tests tabu search on a provided instance to a separate function.

--- Metaheuristic approach/src/main.cpp
+++ Metaheuristic approach/src/main.cpp
@@ -4,23 +4,28 @@
 #include "model.hpp"
 #include "tabu_search.hpp"
 
+void test_tabu_search(const std::string&);
+
 int main(int argc, char** argv) {
 	if (argc != 2) {
 		std::cout << "Usage: " << argv[0] << " <path>" << std::endl;
 		return 1;
 	}
 
-	std::string filepath = argv[1];
+	std::string instance_filepath = argv[1];
+	test_tabu_search(instance_filepath);
 
-	std::cout << filepath << std::endl;
+	return 0;
+}
+
+void test_tabu_search(const std::string& instance_filepath) {
+	std::cout << instance_filepath << std::endl;
 
 	RandomGenerator::set_seed(123);
-	Model::create_model(filepath);
+	Model::create_model(instance_filepath);
 	
 	auto [result, fitness] = TabuSearcher::tabu_search();
 
 	std::cout << "Best solution: " << result << std::endl;
 	std::cout << "Obj: " << fitness << std::endl;
-
-	return 0;
 }
