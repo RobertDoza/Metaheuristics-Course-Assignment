8fafa5630b9072c68e39f7e1ea1c58a192ce6d99
Robert <dozarobert98@gmail.com>
2025-10-18 14:04:37 +0200

    Change MOV_TL_SIZE to 15

--- Metaheuristic approach/include/tabu_list.hpp
+++ Metaheuristic approach/include/tabu_list.hpp
@@ -6,7 +6,7 @@
 #include "movement.hpp"
 
 constexpr unsigned TABU_LIST_SIZE = 10;
-constexpr unsigned MOV_TL_SIZE = 1500;
+constexpr unsigned MOV_TL_SIZE = 15;
 
 struct TabuListEntry {
     Solution solution;
