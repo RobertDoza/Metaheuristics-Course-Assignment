69ca149115ac44ef7a4b4a09c22b2d6c1ec31eec
Robert <dozarobert98@gmail.com>
2025-10-20 20:11:21 +0200

    Modify local search parameters

--- Metaheuristic approach/src/tabu_search.cpp
+++ Metaheuristic approach/src/tabu_search.cpp
@@ -160,7 +160,7 @@ std::optional<LocalSearchResult> TabuSearcher::get_local_best_solution(const Sol
 
 	int counter = 0;
 	int p = Model::get_target_num_sites();
-	auto neighbor_iterator = N3NeighborIterator(solution, p/8, 8);
+	auto neighbor_iterator = N3NeighborIterator(solution, 8, p / 4);
 
 	while (true) {
 		counter++;
